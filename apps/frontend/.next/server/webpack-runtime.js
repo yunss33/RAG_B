9d9f4128c6530f1601e512e865aaff73b0880ca9
yunss33 <yunss33@users.noreply.github.com>
2026-04-13 09:59:29 +0000

    feat: 重新设计 Atypica.ai 网站
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

--- apps/frontend/.next/server/webpack-runtime.js
+++ apps/frontend/.next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("59076df36748f354")
+/******/ 		__webpack_require__.h = () => ("2ed949472fb0e092")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
